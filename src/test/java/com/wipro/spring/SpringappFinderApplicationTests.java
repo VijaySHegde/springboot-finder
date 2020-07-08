80929f2b117c6b0fe0505ce8202110d384bbade1
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-07-08 17:21:39 +0530

    Update SpringappFinderApplicationTests.java

--- src/test/java/com/wipro/spring/SpringappFinderApplicationTests.java
+++ src/test/java/com/wipro/spring/SpringappFinderApplicationTests.java
@@ -3,11 +3,37 @@ package com.wipro.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.jupiter.api.Order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import com.wipro.spring.repo.repo;
+
+
+
 @SpringBootTest
 class SpringappFinderApplicationTests {
+	@Autowired
+    repo rep;
 
-	@Test
-	void contextLoads() {
-	}
-
+	
+	 @Test
+	    @Rollback(false)
+	    @Order(1)
+	    public void testDeleteProduct() {
+	    	String emp_id = "pr20091077";
+	    	
+	    	boolean isExistBeforDelete = rep.findById(emp_id).isPresent();
+	    	
+	    	assertTrue(isExistBeforDelete);
+	    	
+	         
+	    }
+	   
 }
